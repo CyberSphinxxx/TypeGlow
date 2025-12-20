325f2c1beed740b57dd4aed8ece66bde482763c4
CyberSphinxx <johnlemargonzales@gmail.com>
2025-12-20 09:47:51 +0800

    feat: add dedicated leaderboard route page

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import Leaderboard from "$lib/components/Leaderboard.svelte";
+</script>
+
+<Header />
+
+<main
+    class="flex-grow flex flex-col justify-center items-center w-full max-w-[1200px] relative z-10 p-5 mt-20"
+>
+    <Leaderboard />
+</main>
+
+<Footer />
